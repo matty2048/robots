6d6513e6ef7eb4145fbb2bcbfaa6c9de631d9cef
Hashim <hashimafzal0@gmail.com>
2023-12-08 10:53:24 +0000

    Added comments and cleaned up main.py and mapping.py

--- minitask5/scripts/main.py
+++ minitask5/scripts/main.py
@@ -1,15 +1,7 @@
 import rospy
-import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from math import radians, degrees
-from actionlib_msgs.msg import *
-from geometry_msgs.msg import Point
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry, OccupancyGrid, MapMetaData
-from sensor_msgs.msg import LaserScan, Image
+from nav_msgs.msg import Odometry, OccupancyGrid
 from minitask5.msg import image_proc, object_data
 import tf
-import numpy as np
 import math
 
 class Position:
@@ -29,6 +21,7 @@ class Main:
         self.green_tolerance = rospy.get_param('/main/green_tolerance')
         self.red_radius = rospy.get_param('/main/red_radius')
         self.green_radius = rospy.get_param('/main/green_radius')
+        self.obj_detection_range = rospy.get_param('/main/detection_range')
 
         # Variable initialisation
         self.objects_found: object_data = []
@@ -52,6 +45,8 @@ class Main:
 
         # Create main node
         rospy.init_node('main', anonymous=False)
+
+        # Set up shutdown callback
         self.node_name= rospy.get_name()
         rospy.on_shutdown(self.callback_shutdown) 
 
@@ -72,26 +67,31 @@ class Main:
         self.pos.x = msg.pose.pose.position.x
         self.pos.y = msg.pose.pose.position.y
 
+    # Goes through all objects received from /image_proc and checks if they are duplicate objects and valid objects 
     def add_objects_to_list(self):
         for obj in self.objects_found:
             obj: object_data
+            # Checks obj colour
+            # Checks if it has already been added
+            # Checks if it's in the map
+            # Checks if it's close enough to the robot to be counted
             if obj.red > 200:
                 if self.check_obj_in_list(obj, self.red_obj, self.red_tolerance): continue
-                if not self.check_obj_in_map(obj, self.grid, self.red_radius): continue
-                if self.check_obj_dist_to_robot(obj, self.pos) > 1: continue
+                if not self.check_obj_in_map(obj, self.grid, int(self.red_radius/self.resolution)): continue
+                if self.check_obj_dist_to_robot(obj, self.pos) > self.obj_detection_range: continue
                 self.red_obj.append(obj)
             if obj.green > 200:
                 if self.check_obj_in_list(obj, self.green_obj, self.green_tolerance): continue
-                print("green obj not in list")
-                if not self.check_obj_in_map(obj, self.grid, self.red_radius): continue
-                print("green obj in map")
-                if self.check_obj_dist_to_robot(obj, self.pos) > 1: continue
-                print("green obj close to robot")
+                if not self.check_obj_in_map(obj, self.grid, int(self.green_radius/self.resolution)): continue
+                if self.check_obj_dist_to_robot(obj, self.pos) > self.obj_detection_range: continue
                 self.green_obj.append(obj)
     
     def check_dist_between(self, obj: object_data, obj2: object_data):
         return math.dist((obj.x_location, obj.y_location), (obj2.x_location, obj2.y_location))
 
+    # Checks if the object is already within the list within a given distance tolerance (m)
+    # Returns true if the list is exhausted
+    # Returns false if an object is found
     def check_obj_in_list(self, obj: object_data, obj_list: object_data, tolerance):
         if len(obj_list) == 0: return False
         else:
@@ -99,14 +99,17 @@ class Main:
                 if self.check_dist_between(obj, obj_list[i]) < tolerance: return False
                 elif i == len(obj_list) - 1 : return True
     
+    # Checks if the object is already within the list within a given radius from it's stated location (m/res)
+    # Returns true if the object is in the map
+    # Returns false if an object is not found in the map
     def check_obj_in_map(self, obj: object_data, map_data, radius):
         coords = self.to_grid(obj.x_location, obj.y_location, self.origin, self.resolution)
-        # idx = self.to_index(coords[0], coords[1], self.width)
-        size_r = int(radius/(2*self.resolution))
+        size_r = radius
         count = 0
         for i in range(-size_r, size_r):
             for j in range(-size_r, size_r):
                 if map_data[self.to_index(coords[0] + i, coords[1] + j, self.width)] > 65: count += 1
+        # Compares the ratio of tiles discovered to undiscovered in that region
         if count / math.pow(size_r, 2) > 0.1: return True
         else: return False
 
@@ -126,6 +129,8 @@ class Main:
     def to_index(self, gx, gy, size_x):
         return gy * size_x + gx
 
+    # Function to run when the node is shutdown
+    # Prints out the objects that it found during it's exploration of the area
     def callback_shutdown(self):
         print(self.node_name + " shutting down")
         print("Found {r_len} number of red objects and {g_len} number of green objects".format(r_len= len(self.red_obj), g_len= len(self.green_obj)))
@@ -138,7 +143,7 @@ class Main:
 
     def run(self):
         r = rospy.Rate(self.SLEEP_RATE)
-        # Whilst not shutdown OR Found all objects
+        # Flags for printing when it's found the correct number of objects
         found_red = False
         found_green = False
         while not rospy.is_shutdown():
